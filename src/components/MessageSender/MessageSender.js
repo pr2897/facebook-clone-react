4b26f8ef3eccc4d99971c8454fd2577da63bd9e4
Piyush Raj <piyushrajkhg@gmail.com>
2020-08-31 16:07:00 +0530

    only post update to be done

--- src/components/MessageSender/MessageSender.js
+++ src/components/MessageSender/MessageSender.js
@@ -6,80 +6,42 @@ import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
-class MessageSender extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: '',
-      imageURL: '',
-    };
-    this.inputChanged = this.inputChanged.bind(this);
-    this.imageURLChanged = this.imageURLChanged.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  inputChanged = e => {
-    this.setState({
-      input: e.target.value,
-    });
-  };
-
-  imageURLChanged = e => {
-    this.setState({
-      imageURL: e.target.value,
-    });
-  };
-
-  handleSubmit = e => {
-    // handle db operations
-
-    // over here
-
-    // reset state
-    this.setState({
-      input: '',
-      imageURL: '',
-    });
-    e.preventDefault();
-  };
-
-  render() {
-    return (
-      <div className="messageSender">
-        <div className="messageSender__top">
-          <Avatar />
-          <form>
-            <input
-              onChange={this.inputChanged}
-              value={this.state.input}
-              className="messageSender__input"
-              placeholder={`What's on your mind`}
-            />
-            <input
-              value={this.state.imageURL}
-              onChange={this.imageURLChanged}
-              placeholder={`Image URL (Optional)`}
-            />
-            <button onClick={this.handleSubmit}>Hidden Button</button>
-          </form>
+function MessageSender(props) {
+  return (
+    <div className="messageSender">
+      <div className="messageSender__top">
+        <Avatar />
+        <form>
+          <input
+            onChange={props.inputChanged}
+            value={props.state.input}
+            className="messageSender__input"
+            placeholder={`What's on your mind`}
+          />
+          <input
+            value={props.state.imageURL}
+            onChange={props.imageURLChanged}
+            placeholder={`Image URL (Optional)`}
+          />
+          <button onClick={props.handleSubmit}>Hidden Button</button>
+        </form>
+      </div>
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: 'red' }} />
+          <h3>Live Video</h3>
         </div>
-        <div className="messageSender__bottom">
-          <div className="messageSender__option">
-            <VideocamIcon style={{ color: 'red' }} />
-            <h3>Live Video</h3>
-          </div>
-          <div className="messageSender__option">
-            <PhotoLibraryIcon style={{ color: 'green' }} />
-            <h3>Photos/Video</h3>
-          </div>
-          <div className="messageSender__option">
-            <InsertEmoticonIcon style={{ color: 'orange' }} />
-            <h3>Feeling/Activity</h3>
-          </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: 'green' }} />
+          <h3>Photos/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: 'orange' }} />
+          <h3>Feeling/Activity</h3>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MessageSender;
